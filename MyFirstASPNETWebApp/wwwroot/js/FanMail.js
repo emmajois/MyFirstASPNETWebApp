56af0454b072674a17c4d365672db4514c363834
Emma Gardner <emmajoig@gmail.com>
2022-01-08 15:24:16 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,7 @@
+﻿$("#btnsend").click(function () {
+    alert("From: " + $("#txtfrom").val() +
+        ", Subject: " + $("#txtsubject").val() +
+        ", Message: " + $("#txtmsg").val());
+
+    $("#picGanderson").fadeOut("slow");
+})
